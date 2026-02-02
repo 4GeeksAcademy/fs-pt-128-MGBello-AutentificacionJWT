ba6eca1c5e5a4b6f08e8990b82e2a0bef663304e
MBello21 <magarbello@gmail.com>
2026-02-02 12:00:47 +0000

    login

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select, or_
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    if not data.get('email') or not data.get('username') or not data.get('password'):
+        return jsonify({"Error": "Email, username or password not exist"}), 400
+    user_exist = db.session.execute(select(User).where(
+        User.email == data.get("email"))).scalar_one_or_none()
+
+    if user_exist:
+        return jsonify({"Error": "Email, username or password not exist"}), 400
+
+    new_user = User(
+        username=data.get('username'),
+        firstname=data.get('firstname'),
+        lastname=data.get('lastname'),
+        email=data.get('email')
+    )
+    new_user.generate(data.get('password'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "Login successfully"})
+
+
+@api.route('login', method=['POST'])
+def login():
+    data = request.get_json()
+    if not data.get('email') or not data.get('username') or not data.get('password'):
+        return jsonify({"Error": "Email, username or password not exist"}), 400
+    user = db.session.execute(
+        select(User).where(
+            or_(
+                User.email == data.get("email"),
+                User.username == data.get("username")
+            )
+        )
+    ).scalar_one_or_none()
+
+    if user:
+        return jsonify({"Error": "Email, username or password not exist"}), 400
